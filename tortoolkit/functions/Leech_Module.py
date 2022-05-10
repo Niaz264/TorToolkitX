81012ad8495bd2306f61b2f86cd372f734b79bb6
Agamya Samuel <48693970+agamya-samuel@users.noreply.github.com>
2022-05-10 15:26:55 +0530

    Update Leech_Module.py -> emoji change
    📗◉

--- tortoolkit/functions/Leech_Module.py
+++ tortoolkit/functions/Leech_Module.py
@@ -558,11 +558,11 @@ async def print_files(e, files, thash=None, path=None, size=None):
     msg_li = []
     for i in files.keys():
         link = f"https://t.me/c/{str(chat_id)[4:]}/{files[i]}"
-        if len(msg + f'🚩 <a href="{link}">{i}</a>\n') > 4000:
+        if len(msg + f'📗◉ <a href="{link}">{i}</a>\n') > 4000:
             msg_li.append(msg)
-            msg = f'🚩 <a href="{link}">{i}</a>\n'
+            msg = f'📗◉ <a href="{link}">{i}</a>\n'
         else:
-            msg += f'🚩 <a href="{link}">{i}</a>\n'
+            msg += f'📗◉ <a href="{link}">{i}</a>\n'
 
     for i in msg_li:
         await e.reply(i, parse_mode="html")
